62c7131f90f75f5987a8fd7dbe97eb7512ad7281
jorchg <rushcentro@gmail.com>
2018-01-20 19:04:57 +0100

    Added object view

---
+++ src/components/Object.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>{{ object }}</p>
+  </div>
+</template>
+
+<script>
+import object from '../assets/mocks/object.json';
+
+export default {
+  name: 'Object',
+  data() {
+    return {
+      object,
+    };
+  },
+  props: [
+    'id',
+  ],
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import main from '@/components/Main';
+import object from '@/components/Object';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Main',
       component: main,
+    },
+    {
+      path: '/objeto/:id',
+      name: 'Object',
+      component: object,
     },
   ],
 });
